f74a1c456d85ac99758eeeb6d3567cf3c4efbcc4
Leon Bein <leon.bein@student.hpi.de>
2020-01-10 14:02:42 +0100

    First approach of data relation translation

--- src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/CompilerApp.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/CompilerApp.java
@@ -2,9 +2,17 @@ package de.uni_potsdam.hpi.bpt.fcm2cpn;
 
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.impl.instance.BpmnModelElementInstanceImpl;
+import org.camunda.bpm.model.bpmn.impl.instance.DataOutputImpl;
+import org.camunda.bpm.model.bpmn.impl.instance.SourceRef;
+import org.camunda.bpm.model.bpmn.impl.instance.TargetRef;
 import org.camunda.bpm.model.bpmn.instance.Activity;
+import org.camunda.bpm.model.bpmn.instance.DataAssociation;
 import org.camunda.bpm.model.bpmn.instance.DataObject;
+import org.camunda.bpm.model.bpmn.instance.DataObjectReference;
 import org.camunda.bpm.model.bpmn.instance.DataState;
+import org.camunda.bpm.model.bpmn.instance.DataStore;
+import org.camunda.bpm.model.bpmn.instance.DataStoreReference;
 import org.camunda.bpm.model.bpmn.instance.Event;
 import org.camunda.bpm.model.bpmn.instance.Gateway;
 import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
@@ -29,7 +37,9 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
@@ -66,11 +76,12 @@ public class CompilerApp {
 
     private PetriNet translateBPMN2CPN() throws Exception {
     	initializeCPNModel();
-        translateDataObjects();
+        translateData();
         translateActivites();
         translateEvents();
         translateGateways();
         translateControlFlow();
+        translateDataFlow();
         return petriNet;
     }
     
@@ -78,50 +89,77 @@ public class CompilerApp {
         System.out.print("Initalizing CPN model... ");
         petriNet = builder.createPetriNet();
         mainPage = builder.addPage(petriNet, "Main Page");
-        builder.declareStandardColors(petriNet);
-        builder.declareColorSet(petriNet, "CaseID", CpntypesFactory.INSTANCE.createCPNString());
+        initializeDefaultColorSets();
         System.out.println("DONE");
     }
     
-    private void translateDataObjects() {
-        Collection<DataObject> objectInstances = bpmn.getModelElementsByType(DataObject.class);
-        Set<String> dataObjectNames = objectInstances.stream()
-                .map(obj -> (DataObject)obj)
-                .map(DataObject::getName)
-                .map(s -> s.trim().toLowerCase())
-                .collect(Collectors.toSet());
-        dataObjectNames.forEach(s -> {
-            builder.addPlace(mainPage, s, "DATA_OBJECT");
-        });
-        ModelElementType dataStateType = bpmn.getModel().getType(DataState.class);
-        Collection<ModelElementInstance> dataStates = bpmn.getModelElementsByType(dataStateType);
-        CpntypesFactory cpntypesFactory = CpntypesFactoryImpl.init();
-        CPNEnum cpnEnum = cpntypesFactory.createCPNEnum();
-        CPNRecord dataObject = cpntypesFactory.createCPNRecord();
-        dataStates.stream().map(state -> state.getAttributeValue("name").replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s","")).collect(Collectors.toSet())
-                .forEach(s -> {
-                    if (!s.contains("|")) {
-                        cpnEnum.addValue(s.toUpperCase());
-                    }
-                });
+    private void initializeDefaultColorSets() {
+        builder.declareStandardColors(petriNet);
+        builder.declareColorSet(petriNet, "CaseID", CpntypesFactory.INSTANCE.createCPNString());
+        builder.declareColorSet(petriNet, "DATA_STORE", CpntypesFactory.INSTANCE.createCPNString());
+        
+        CPNEnum cpnEnum = CpntypesFactory.INSTANCE.createCPNEnum();
+        Collection<DataState> dataStates = bpmn.getModelElementsByType(DataState.class);
+        dataStates.stream()
+        	.map(state -> dataObjectStateToNetColor(state.getAttributeValue("name")))
+            .forEach(cpnEnum::addValue);
         builder.declareColorSet(petriNet, "STATE", cpnEnum);
+        
+        CPNRecord dataObject = CpntypesFactory.INSTANCE.createCPNRecord();
         dataObject.addValue("id", "STRING");
         dataObject.addValue("caseId", "STRING");
         dataObject.addValue("state", "STATE");
         builder.declareColorSet(petriNet, "DATA_OBJECT", dataObject);
     }
     
+    private static String dataObjectStateToNetColor(String state) {
+    	return state.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s","_").toUpperCase();
+    }
+    
+    private void translateData() {
+        translateDataObjects();        
+        translateDataStores();
+    }
+    
+    
+    private void translateDataObjects() {
+    	Collection<DataObject> dataObjects = bpmn.getModelElementsByType(DataObject.class);
+        Map<DataObject, Node> dataObjectsToPlaces = new HashMap<>();
+        dataObjects.forEach(each -> {
+        	Node node = builder.addPlace(mainPage, each.getName().trim().toLowerCase(), "DATA_OBJECT");
+        	dataObjectsToPlaces.put(each, node);
+        });
+        
+        Collection<DataObjectReference> dataObjectRefs = bpmn.getModelElementsByType(DataObjectReference.class);
+        dataObjectRefs.forEach(each -> {
+        	idsToNodes.put(each.getId(), dataObjectsToPlaces.get(each.getDataObject()));
+        });
+    }
+    
+    private void translateDataStores() {
+        Collection<DataStore> dataStores = bpmn.getModelElementsByType(DataStore.class);
+        Map<DataStore, Node> dataStoresToPlaces = new HashMap<>();
+        dataStores.forEach(each -> {
+        	Node node = builder.addPlace(mainPage, each.getName().trim().toLowerCase(), "DATA_STORE");
+        	dataStoresToPlaces.put(each, node);
+        });
+        Collection<DataStoreReference> dataStoreRefs = bpmn.getModelElementsByType(DataStoreReference.class);
+        dataStoreRefs.forEach(each -> {
+        	idsToNodes.put(each.getId(), dataStoresToPlaces.get(each.getDataStore()));
+        });
+    }
+    
     private void translateActivites() {
         Collection<Activity> activities = bpmn.getModelElementsByType(Activity.class);
-        activities.stream()
-        	.forEach(each -> {
-            	String name = each.getName();
-	        	Page activityPage = builder.addPage(petriNet, normalizeActivityName(name));
-	        	builder.addTransition(activityPage, name);
-	            Node node = builder.createSubPageTransition(activityPage, mainPage, name);
-	            activityPages.put(name, activityPage);
-	            idsToNodes.put(each.getId(), node);
-	        });
+        activities.forEach(each -> {
+        	String name = each.getName();
+        	Page activityPage = builder.addPage(petriNet, normalizeActivityName(name));
+        	builder.addTransition(activityPage, name);
+            //TODO Node node = builder.createSubPageTransition(activityPage, mainPage, name);
+        	Node node = builder.addTransition(mainPage, name);
+            activityPages.put(name, activityPage);
+            idsToNodes.put(each.getId(), node);
+        });
     }
     
     private String normalizeActivityName(String name) {
@@ -130,7 +168,7 @@ public class CompilerApp {
     
     private void translateEvents() {
         Collection<Event> events = bpmn.getModelElementsByType(Event.class);
-        events.stream().forEach(each -> {
+        events.forEach(each -> {
         	Node node = builder.addTransition(mainPage, each.getName());
         	idsToNodes.put(each.getId(), node);
         });
@@ -139,7 +177,7 @@ public class CompilerApp {
     private void translateGateways() {
     	//TODO only works for Xor Gateways
         Collection<Gateway> gateways = bpmn.getModelElementsByType(Gateway.class);
-        gateways.stream().forEach(each -> {
+        gateways.forEach(each -> {
         	Node node = builder.addPlace(mainPage, each.getName(), "CaseID");
         	idsToNodes.put(each.getId(), node);
         });
@@ -147,7 +185,7 @@ public class CompilerApp {
     
     private void translateControlFlow() {
         Collection<SequenceFlow> sequenceFlows = bpmn.getModelElementsByType(SequenceFlow.class);
-        sequenceFlows.stream().forEach(each -> {
+        sequenceFlows.forEach(each -> {
         	Node source = idsToNodes.get(each.getSource().getId());
         	Node target = idsToNodes.get(each.getTarget().getId());
         	if(isPlace(source) || isPlace(target)) {
@@ -158,6 +196,37 @@ public class CompilerApp {
             	builder.addArc(mainPage, place, target, "");
         	}
         });
+    }
+    
+    private static <T extends ModelElementInstance> List<String> getReferenceIds(DataAssociation association, Class<T> referenceClass) {
+		return association.getChildElementsByType(referenceClass).stream()
+				.map(each -> each.getTextContent())
+				.collect(Collectors.toList());
+    }
+    
+    private Node findKnownParentNode(String sourceId) {
+    	ModelElementInstance element = bpmn.getModelElementById(sourceId);
+    	String id = sourceId;
+    	while(element != null && !idsToNodes.containsKey(id)) {
+    		element = element.getParentElement();
+    		id = element.getAttributeValue("id");
+    	}
+    	if(element != null)return idsToNodes.get(id);
+    	else throw new Error("Could not find known parent element for element with id "+id);
+		
+    }
+    
+    private void translateDataFlow() {
+    	Collection<DataAssociation> dataAssociations = bpmn.getModelElementsByType(DataAssociation.class);
+    	dataAssociations.forEach(each -> {
+    		List<String> sourceIds = getReferenceIds(each, SourceRef.class);
+    		List<String> targetIds = getReferenceIds(each, TargetRef.class);
+    		assert sourceIds.size() == 1 && targetIds.size() == 1;
+    		Node sourceNode = findKnownParentNode(sourceIds.get(0));
+    		Node targetNode = findKnownParentNode(targetIds.get(0));
+    		assert Objects.nonNull(sourceNode) && Objects.nonNull(targetNode);
+        	builder.addArc(mainPage, sourceNode, targetNode, "");
+    	});
     }
     
     private static boolean isPlace(Node node) {
