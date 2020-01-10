b25609131fd96b78405a95b344a0e8a391bc49b1
Leon Bein <leon.bein@student.hpi.de>
2020-01-10 11:12:42 +0100

    Added initial control flow

--- src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/CompilerApp.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/CompilerApp.java
@@ -5,12 +5,18 @@ import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.instance.Activity;
 import org.camunda.bpm.model.bpmn.instance.DataObject;
 import org.camunda.bpm.model.bpmn.instance.DataState;
+import org.camunda.bpm.model.bpmn.instance.Event;
+import org.camunda.bpm.model.bpmn.instance.Gateway;
 import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 import org.camunda.bpm.model.xml.type.ModelElementType;
 import org.cpntools.accesscpn.model.ModelPrinter;
+import org.cpntools.accesscpn.model.Node;
 import org.cpntools.accesscpn.model.Page;
 import org.cpntools.accesscpn.model.PetriNet;
+import org.cpntools.accesscpn.model.Place;
+import org.cpntools.accesscpn.model.Transition;
+import org.cpntools.accesscpn.model.TransitionNode;
 import org.cpntools.accesscpn.model.cpntypes.CPNEnum;
 import org.cpntools.accesscpn.model.cpntypes.CPNRecord;
 import org.cpntools.accesscpn.model.cpntypes.CpntypesFactory;
@@ -20,7 +26,9 @@ import org.cpntools.accesscpn.model.util.BuildCPNUtil;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -31,6 +39,8 @@ public class CompilerApp {
 	private BpmnModelInstance bpmn;
 	private BuildCPNUtil builder;
 	private PetriNet petriNet;
+	private Map<String, Page> activityPages;
+	private Map<String, Node> idsToNodes;
 
     public static void main(final String[] args) throws Exception {
         BpmnModelInstance bpmn = loadBPMNFile("./src/main/resources/cat_example.bpmn");
@@ -43,6 +53,8 @@ public class CompilerApp {
     private CompilerApp(BpmnModelInstance bpmn) {
     	this.bpmn = bpmn;
         this.builder = new BuildCPNUtil();
+        this.activityPages = new HashMap<>();
+        this.idsToNodes = new HashMap<>();
 	}
     
     private static BpmnModelInstance loadBPMNFile(String bpmnFileUri) {
@@ -56,11 +68,9 @@ public class CompilerApp {
     	initializeCPNModel();
         translateDataObjects();
         translateActivites();
-        ModelElementType sequenceFlowType = bpmn.getModel().getType(SequenceFlow.class);
-        Collection<ModelElementInstance> sequenceFlows = bpmn.getModelElementsByType(sequenceFlowType);
-        sequenceFlows.forEach(sf -> {
-
-        });
+        translateEvents();
+        translateGateways();
+        translateControlFlow();
         return petriNet;
     }
     
@@ -69,12 +79,12 @@ public class CompilerApp {
         petriNet = builder.createPetriNet();
         mainPage = builder.addPage(petriNet, "Main Page");
         builder.declareStandardColors(petriNet);
+        builder.declareColorSet(petriNet, "CaseID", CpntypesFactory.INSTANCE.createCPNString());
         System.out.println("DONE");
     }
     
     private void translateDataObjects() {
-        ModelElementType objectType = bpmn.getModel().getType(DataObject.class);
-        Collection<ModelElementInstance> objectInstances = bpmn.getModelElementsByType(objectType);
+        Collection<DataObject> objectInstances = bpmn.getModelElementsByType(DataObject.class);
         Set<String> dataObjectNames = objectInstances.stream()
                 .map(obj -> (DataObject)obj)
                 .map(DataObject::getName)
@@ -102,14 +112,15 @@ public class CompilerApp {
     }
     
     private void translateActivites() {
-        ModelElementType activityType = bpmn.getModel().getType(Activity.class);
-        Collection<ModelElementInstance> activities = bpmn.getModelElementsByType(activityType);
+        Collection<Activity> activities = bpmn.getModelElementsByType(Activity.class);
         activities.stream()
-        	.map(a -> a.getAttributeValue("name"))
         	.forEach(each -> {
-	        	Page activityPage = builder.addPage(petriNet, normalizeActivityName(each));
-	        	builder.addTransition(activityPage, each);
-	            builder.createSubPageTransition(activityPage, mainPage, each);
+            	String name = each.getName();
+	        	Page activityPage = builder.addPage(petriNet, normalizeActivityName(name));
+	        	builder.addTransition(activityPage, name);
+	            Node node = builder.createSubPageTransition(activityPage, mainPage, name);
+	            activityPages.put(name, activityPage);
+	            idsToNodes.put(each.getId(), node);
 	        });
     }
     
@@ -117,6 +128,45 @@ public class CompilerApp {
     	return name.replace('\n', ' ');
     }
     
+    private void translateEvents() {
+        Collection<Event> events = bpmn.getModelElementsByType(Event.class);
+        events.stream().forEach(each -> {
+        	Node node = builder.addTransition(mainPage, each.getName());
+        	idsToNodes.put(each.getId(), node);
+        });
+    }
+    
+    private void translateGateways() {
+    	//TODO only works for Xor Gateways
+        Collection<Gateway> gateways = bpmn.getModelElementsByType(Gateway.class);
+        gateways.stream().forEach(each -> {
+        	Node node = builder.addPlace(mainPage, each.getName(), "CaseID");
+        	idsToNodes.put(each.getId(), node);
+        });
+    }
+    
+    private void translateControlFlow() {
+        Collection<SequenceFlow> sequenceFlows = bpmn.getModelElementsByType(SequenceFlow.class);
+        sequenceFlows.stream().forEach(each -> {
+        	Node source = idsToNodes.get(each.getSource().getId());
+        	Node target = idsToNodes.get(each.getTarget().getId());
+        	if(isPlace(source) || isPlace(target)) {
+            	builder.addArc(mainPage, source, target, "");
+        	} else {
+            	Node place = builder.addPlace(mainPage, null, "CaseID");
+            	builder.addArc(mainPage, source, place, "");
+            	builder.addArc(mainPage, place, target, "");
+        	}
+        });
+    }
+    
+    private static boolean isPlace(Node node) {
+    	return node instanceof Place;
+    }
+    
+    private static boolean isTransition(Node node) {
+    	return node instanceof Transition;
+    }
 
     private static class ActivityNode {
             String name;
