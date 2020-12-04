6c9d50e08fcb3677c924b08432e754319b534c9f
Leon Bein <leon.bein@student.hpi.de>
2020-12-04 14:25:17 +0100

    Add red test for: OLCParser is ignores IOSets

--- src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/OLCParserTests.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/OLCParserTests.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static de.uni_potsdam.hpi.bpt.fcm2cpn.utils.Utils.*;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 
 import org.camunda.bpm.model.bpmn.instance.Activity;
 import org.camunda.bpm.model.bpmn.instance.DataObject;
@@ -12,6 +14,7 @@ import org.camunda.bpm.model.bpmn.instance.DataObject;
 import de.uni_potsdam.hpi.bpt.fcm2cpn.dataModel.ObjectLifeCycle;
 import de.uni_potsdam.hpi.bpt.fcm2cpn.dataModel.ObjectLifeCycleParser;
 import de.uni_potsdam.hpi.bpt.fcm2cpn.testUtils.ForEachBpmn;
+import de.uni_potsdam.hpi.bpt.fcm2cpn.utils.Pair;
 
 public class OLCParserTests extends ModelStructureTests {
 	
@@ -22,12 +25,6 @@ public class OLCParserTests extends ModelStructureTests {
 		this.olcs = ObjectLifeCycleParser.getOLCs(dataModel, bpmn);
 	}    
 	
-	public ObjectLifeCycle olcFor(String dataObject) {
-    	return Arrays.stream(olcs)
-    			.filter(olc -> olc.getClassName().equals(dataObject))
-    			.findAny()
-    			.get();
-    }
 	
 	@TestWithAllModels
 	@ForEachBpmn(DataObject.class)
@@ -38,19 +35,54 @@ public class OLCParserTests extends ModelStructureTests {
 	
 	@TestWithAllModels
 	@ForEachBpmn(Activity.class)
-	public void testAllTransitionsByActivityArePossible(Activity activity) {
-		expectedIOCombinations(activity).forEach(ioCombination -> {
-			ioCombination.first.forEach((object, inputState) -> {
-				if(ioCombination.second.containsKey(object)) {
-					Optional<String> outputState = ioCombination.second.get(object);
-					if(!inputState.equals(outputState)) {
-						ObjectLifeCycle olc = olcFor(object);
-						assertTrue(olc.getState(inputState.get()).get().getSuccessors().contains(olc.getState(outputState.get()).get()), 
-								"Olc does not support lifecycle transition ("+inputState+" -> "+outputState+") for data object "+object+" which is definied by activity "+activity.getName());
-					}
+	@ForEachIOSet
+	public void testAllTransitionsByActivityArePossible(Activity activity, DataObjectIOSet ioSet) {
+		Pair<Map<String, Optional<String>>, Map<String, Optional<String>>> ioCombination = ioAssociationsToStateMaps(ioSet);
+		ioCombination.first.forEach((object, inputState) -> {
+			if(ioCombination.second.containsKey(object)) {
+				Optional<String> outputState = ioCombination.second.get(object);
+				if(!inputState.equals(outputState)) {
+					ObjectLifeCycle olc = olcFor(object);
+					assertTrue(olc.getState(inputState.get()).get().getSuccessors().contains(olc.getState(outputState.get()).get()), 
+							"Olc does not support lifecycle transition ("+inputState+" -> "+outputState+") for data object "+object+" which is definied by activity "+activity.getName());
 				}
-			});
+			}
 		});
 	}
+	
+	@TestWithAllModels
+	public void testAllOLCStateTransitionsAreValid() {
+		forEachOLCTransition((olc, states) -> {
+			String dataObject = olc.getClassName();
+			boolean anyActivityCanTakeThisTransition = bpmn.getModelElementsByType(Activity.class).stream().anyMatch(activity -> {
+				return ioAssociationCombinations(activity).stream().anyMatch(ioSet -> 
+				ioSet.first.stream().anyMatch(inputAssoc -> inputAssoc.dataElementName().equals(dataObject) && states.first.getStateName().equals(inputAssoc.getStateName().orElse(null)))
+				&& ioSet.second.stream().anyMatch(outputAssoc -> outputAssoc.dataElementName().equals(dataObject) && states.second.getStateName().equals(outputAssoc.getStateName().orElse(null)))
+				);
+			});
+			assertTrue(anyActivityCanTakeThisTransition, "There is no activity which changes state of '"+dataObject+"' from "+states.first.getStateName()+" to "+states.second.getStateName());
+		});
+	}
+	
+	
+//=========== Utility ================
+	
+	public ObjectLifeCycle olcFor(String dataObject) {
+    	return Arrays.stream(olcs)
+    			.filter(olc -> olc.getClassName().equals(dataObject))
+    			.findAny()
+    			.get();
+    }
+	
+	public void forEachOLCTransition(BiConsumer<ObjectLifeCycle, Pair<ObjectLifeCycle.State, ObjectLifeCycle.State>> consumer) {
+		for(ObjectLifeCycle olc : olcs) {
+			for(ObjectLifeCycle.State startState : olc.getStates()) {
+				for(ObjectLifeCycle.State targetState : startState.getSuccessors()) {
+					consumer.accept(olc, new Pair<>(startState, targetState));
+				}
+			}
+		}
+	}
+	
 
 }
