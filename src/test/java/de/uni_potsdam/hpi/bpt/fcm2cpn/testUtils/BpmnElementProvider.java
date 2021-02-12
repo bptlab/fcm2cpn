46fa51783c7aab83b9a68bc82066334bfbf68207
Leon Bein <leon.bein@student.hpi.de>
2021-02-12 15:12:10 +0100

    [Minor] remove unused import

--- src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/testUtils/BpmnElementProvider.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/testUtils/BpmnElementProvider.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.support.AnnotationConsumer;
 
-import de.uni_potsdam.hpi.bpt.fcm2cpn.ModelStructureTests;
-
 public class BpmnElementProvider implements ArgumentsProvider, AnnotationConsumer<ForEachBpmn>, ArgumentContextNameResolver {
 	
 	private Class<? extends BaseElement> elementClass;
