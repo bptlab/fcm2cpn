633113e5fadc53f3657e8c161c3f11df40b4affa
Leon Bein <leon.bein@student.hpi.de>
2021-02-19 13:30:17 +0100

    Refined creation checking revealing missing upper bounds check

--- src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/utils/DataObjectIOSet.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/utils/DataObjectIOSet.java
@@ -1,6 +1,7 @@
 package de.uni_potsdam.hpi.bpt.fcm2cpn.utils;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.camunda.bpm.model.bpmn.instance.DataInputAssociation;
 import org.camunda.bpm.model.bpmn.instance.DataObjectReference;
@@ -17,6 +18,47 @@ public class DataObjectIOSet extends Pair<List<StatefulDataAssociation<DataInput
 		super(first, second);
 	}
 
+	public boolean reads(String dataObject) {
+		return readsAsCollection(dataObject) || readsAsNonCollection(dataObject);
+	}
+
+	public boolean readsAsCollection(String dataObject) {
+		return inputsForName(dataObject).anyMatch(StatefulDataAssociation::isCollection);
+	}
+
+	public boolean readsAsNonCollection(String dataObject) {
+		return inputsForName(dataObject).anyMatch(each -> !each.isCollection());
+	}
+
+	public boolean readsInState(String dataObject, State state) {
+		return inputsForName(dataObject).anyMatch(each -> state.getOLC().getState(each.getStateName()).get().equals(state));
+	}
+
+	public boolean writes(String dataObject) {
+		return writesAsNonCollection(dataObject) || writesAsCollection(dataObject);
+	}
+
+	public boolean writesAsCollection(String dataObject) {
+		return outputsForName(dataObject).anyMatch(StatefulDataAssociation::isCollection);
+	}
+
+	public boolean writesAsNonCollection(String dataObject) {
+		return outputsForName(dataObject).anyMatch(each -> !each.isCollection());
+	}
+
+	public boolean writesInState(String dataObject, State state) {
+		return outputsForName(dataObject).anyMatch(each -> state.getOLC().getState(each.getStateName()).get().equals(state));
+	}
+
+	public boolean creates(String dataObject) {
+		return writesAsNonCollection(dataObject) && !readsAsNonCollection(dataObject);
+	}
+
+	public boolean createsAssociationBetween(String first, String second) {
+		return reads(first) && creates(second) || reads(second) && creates(first);
+	}
+	
+
 	public boolean associationShouldAlreadyBeInPlace(String firstDataObject, String secondDataObject) {
 		return reads(firstDataObject) && reads(secondDataObject)
 				&& (!writes(firstDataObject)
@@ -26,46 +68,13 @@ public class DataObjectIOSet extends Pair<List<StatefulDataAssociation<DataInput
 						|| readsAsCollection(secondDataObject) == writesAsCollection(secondDataObject)
 								&& readsAsNonCollection(secondDataObject) == writesAsNonCollection(secondDataObject));
 	}
-
-	public boolean reads(String dataObject) {
-		return first.stream().anyMatch(each -> each.dataElementName().equals(dataObject));
+	
+	
+	private Stream<StatefulDataAssociation<DataInputAssociation, DataObjectReference>> inputsForName(String dataObject) {
+		return first.stream().filter(each -> each.dataElementName().equals(dataObject));
 	}
-
-	public boolean readsAsCollection(String dataObject) {
-		return first.stream().anyMatch(each -> each.dataElementName().equals(dataObject) && each.isCollection());
-	}
-
-	public boolean readsAsNonCollection(String dataObject) {
-		return first.stream().anyMatch(each -> each.dataElementName().equals(dataObject) && !each.isCollection());
-	}
-
-	public boolean readsInState(String dataObject, State state) {
-		return first.stream().anyMatch(each -> each.dataElementName().equals(dataObject)
-				&& state.getOLC().getState(each.getStateName()).get().equals(state));
-	}
-
-	public boolean writes(String dataObject) {
-		return second.stream().anyMatch(each -> each.dataElementName().equals(dataObject));
-	}
-
-	public boolean writesAsCollection(String dataObject) {
-		return second.stream().anyMatch(each -> each.dataElementName().equals(dataObject) && each.isCollection());
-	}
-
-	public boolean writesAsNonCollection(String dataObject) {
-		return second.stream().anyMatch(each -> each.dataElementName().equals(dataObject) && !each.isCollection());
-	}
-
-	public boolean writesInState(String dataObject, State state) {
-		return second.stream().anyMatch(each -> each.dataElementName().equals(dataObject)
-				&& state.getOLC().getState(each.getStateName()).get().equals(state));
-	}
-
-	public boolean creates(String dataObject) {
-		return writes(dataObject) && !reads(dataObject); //writesAsNonCollection(dataObject) && !readsAsNonCollection(dataObject);
-	}
-
-	public boolean createsAssociationBetween(String first, String second) {
-		return reads(first) && creates(second) || reads(second) && creates(first);
+	
+	private Stream<StatefulDataAssociation<DataOutputAssociation, DataObjectReference>> outputsForName(String dataObject) {
+		return second.stream().filter(each -> each.dataElementName().equals(dataObject));
 	}
 }
