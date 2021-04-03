f701657b602a054360ebdf470cfc05658e872777
Leon Bein <leon.bein@student.hpi.de>
2021-04-03 14:55:53 +0200

    Add basic tests for termination condition compilation

--- src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/terminationconditions/TerminationLiteral.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/fcm2cpn/terminationconditions/TerminationLiteral.java
@@ -1,6 +1,11 @@
 package de.uni_potsdam.hpi.bpt.fcm2cpn.terminationconditions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class TerminationLiteral {
 	
@@ -23,6 +28,10 @@ public class TerminationLiteral {
 
 	public Set<String> getStates() {
 		return states;
+	}
+	
+	public static Collector<TerminationLiteral, ?, Map<String, List<String>>> stateMapCollector() {
+		return Collectors.toMap(TerminationLiteral::getDataObject, literal -> new ArrayList<>(literal.getStates()));
 	}
 
 }
--- src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/terminationconditions/TerminationConditionCompilerTests.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/terminationconditions/TerminationConditionCompilerTests.java
@@ -1,15 +1,24 @@
 package de.uni_potsdam.hpi.bpt.fcm2cpn.terminationconditions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.cpntools.accesscpn.model.Instance;
 import org.cpntools.accesscpn.model.Page;
 import org.cpntools.accesscpn.model.Place;
+import org.cpntools.accesscpn.model.Transition;
 
 import de.uni_potsdam.hpi.bpt.fcm2cpn.ModelStructureTests;
 import de.uni_potsdam.hpi.bpt.fcm2cpn.testUtils.TestWithAllModels;
+import de.uni_potsdam.hpi.bpt.fcm2cpn.utils.Utils;
 
 public class TerminationConditionCompilerTests extends ModelStructureTests {
 	
@@ -24,6 +33,26 @@ public class TerminationConditionCompilerTests extends ModelStructureTests {
 		assertEquals(terminationCondition.isPresent(), Objects.nonNull(getTerminationPage()),
 				"Terminated condition page was not created as needed.");
 	}
+	
+	@TestWithAllModels
+	public void testOneTransitionForEachPossibleStateCombination() {
+		assumeTrue(terminationCondition.isPresent(), "Model does not have a termination condition");
+		for(List<TerminationLiteral> clause : terminationCondition.get().getClauses()) {
+			Map<String, List<String>> stateMap = clause.stream().collect(TerminationLiteral.stateMapCollector());
+			List<Map<String, String>> possibleStateCombinationsForClause = Utils.indexedCombinationsOf(stateMap);
+			for(Map<String, String> stateCombination : possibleStateCombinationsForClause) {
+				List<Transition> transitionsThatMatchStateCombination = StreamSupport.stream(getTerminationPage().transition().spliterator(), false).filter(transition -> {
+					return stateCombination.entrySet().stream().allMatch(dataObjectAndState -> {
+						return transition.getTargetArc().stream().filter(arc -> arc.getSource().getName().asString().equals(Utils.dataPlaceName(dataObjectAndState.getKey(), dataObjectAndState.getValue()))).count() == 1;
+					});
+				}).collect(Collectors.toList());
+				assertEquals(1, transitionsThatMatchStateCombination.size(),
+						"There is not exactly one transition for state combination "+stateCombination+" of clause "+clause);
+			}
+		}
+	}
+	
+	//TODO TestAllTransitionsCreateTokenInTerminationPlace
 
 
 	private Place getTerminationPlace() {
@@ -36,5 +65,15 @@ public class TerminationConditionCompilerTests extends ModelStructureTests {
 	
 	private Page getTerminationPage() {
 		return pagesNamed(TerminationConditionCompiler.TERMINATION_TRANSITION_NAME).findAny().orElse(null);
+	}
+	
+	@Override
+	protected Optional<TerminationCondition> parseTerminationCondition() {
+        File terminationConditionFile = new File("./src/test/resources/"+modelName+".json");
+        if(terminationConditionFile.exists()) {
+        	return Optional.of(TerminationConditionParser.parse(terminationConditionFile));
+        } else {
+            return super.parseTerminationCondition();
+        }
 	}
 }
