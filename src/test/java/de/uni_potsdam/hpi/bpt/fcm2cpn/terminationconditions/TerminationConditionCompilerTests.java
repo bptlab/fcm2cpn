65a08a27ef6ea68d8ed80cfb1de3b835370f52af
Leon Bein <leon.bein@student.hpi.de>
2021-04-16 11:14:13 +0200

    Add test for checking termination transitions check goal bounds

--- src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/terminationconditions/TerminationConditionCompilerTests.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/fcm2cpn/terminationconditions/TerminationConditionCompilerTests.java
@@ -2,6 +2,7 @@ package de.uni_potsdam.hpi.bpt.fcm2cpn.terminationconditions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static de.uni_potsdam.hpi.bpt.fcm2cpn.utils.Utils.*;
 
 import java.io.File;
 import java.util.List;
@@ -9,15 +10,19 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import org.camunda.bpm.model.bpmn.instance.DataObject;
 import org.cpntools.accesscpn.model.Instance;
 import org.cpntools.accesscpn.model.Page;
 import org.cpntools.accesscpn.model.Place;
 import org.cpntools.accesscpn.model.Transition;
 
 import de.uni_potsdam.hpi.bpt.fcm2cpn.ModelStructureTests;
+import de.uni_potsdam.hpi.bpt.fcm2cpn.testUtils.ForEachBpmn;
 import de.uni_potsdam.hpi.bpt.fcm2cpn.testUtils.TestWithAllModels;
+import de.uni_potsdam.hpi.bpt.fcm2cpn.utils.CustomCPNFunctions;
 import de.uni_potsdam.hpi.bpt.fcm2cpn.utils.Utils;
 
 public class TerminationConditionCompilerTests extends ModelStructureTests {
@@ -37,6 +42,7 @@ public class TerminationConditionCompilerTests extends ModelStructureTests {
 	@TestWithAllModels
 	public void testOneTransitionForEachPossibleStateCombination() {
 		assumeTrue(terminationCondition.isPresent(), "Model does not have a termination condition");
+		
 		for(List<TerminationLiteral> clause : terminationCondition.get().getClauses()) {
 			Map<String, String> stateMap = clause.stream().collect(TerminationLiteral.stateMapCollector());
 			assertEquals(1, transitionsThatMatchStateCombination(stateMap).size(),
@@ -44,9 +50,30 @@ public class TerminationConditionCompilerTests extends ModelStructureTests {
 		}
 	}
 	
+	@TestWithAllModels
+	@ForEachBpmn(DataObject.class)
+	public void testGoalCardinalitiesAreChecked(DataObject dataObject) {
+		assumeTrue(terminationCondition.isPresent(), "Model does not have a termination condition");
+		
+		String dataObjectName = elementName(dataObject);
+		dataModel.getAssociationsForDataObject(dataObjectName).forEach(assoc -> {
+			String otherDataObjectName = assoc.getOtherEnd(dataObjectName).getDataObject();
+			int bound = assoc.getEnd(dataObjectName).getGoalLowerBound();
+			if(bound > 0) {
+				getClauseTransitions().forEach(transition -> {
+					assertEquals(1, guardsOf(transition).filter(CustomCPNFunctions.enforceGoalLowerBoundForAll(otherDataObjectName+"_list", dataObjectName, bound)::equals).count(), 
+							"Termination clause transition "+transition.getName().asString()+" does not have exactly one guard that checks that at least "
+							+assoc.getEnd(dataObjectName).getGoalLowerBound()+" "+dataObjectName+"(s) are associated with each "+otherDataObjectName);
+				});
+			}
+		});
+	}
+	
 	//TODO TestAllTransitionsCreateTokenInTerminationPlace
 
-
+	
+// ======== Infrastructure ==============================================================================================
+	
 	private Place getTerminationPlace() {
 		return placesNamed(TerminationConditionCompiler.TERMINATION_PLACE_NAME).findAny().orElse(null);
 	}
@@ -59,8 +86,12 @@ public class TerminationConditionCompilerTests extends ModelStructureTests {
 		return pagesNamed(TerminationConditionCompiler.TERMINATION_TRANSITION_NAME).findAny().orElse(null);
 	}
 	
+	private Stream<Transition> getClauseTransitions() {
+		return StreamSupport.stream(getTerminationPage().transition().spliterator(), false);
+	}
+	
 	private List<Transition> transitionsThatMatchStateCombination(Map<String, String> stateMap) {
-		return StreamSupport.stream(getTerminationPage().transition().spliterator(), false).filter(transition -> {
+		return getClauseTransitions().filter(transition -> {
 			return stateMap.entrySet().stream().allMatch(dataObjectAndState -> {
 				return transition.getTargetArc().stream().filter(arc -> arc.getSource().getName().asString().equals(Utils.dataPlaceName(dataObjectAndState.getKey(), dataObjectAndState.getValue()))).count() == 1;
 			});
